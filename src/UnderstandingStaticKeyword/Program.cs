f7c605b6aaff7d511f1acb54d7e459d5a1cbb08c
Amer Bashoeb <abashoeb@nowdigital.com.au>
2015-10-07 16:40:48 +1100

    initial commit

---
+++ src/UnderstandingStaticKeyword/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnderstandingStaticKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float area1 = Circle.CalculateArea(1);
+            Console.WriteLine("Area = {0}", area1);
+
+            float area2 = Circle.CalculateArea(1);
+            Console.WriteLine("Area = {0}", area2);
+        }
+    }
+
+
+    public static class Circle
+    {
+        static float _Pi = 3.141F;
+        public static int _radius;
+
+        static Circle()
+        {
+            _radius = 2;
+        }
+
+        public static float CalculateArea(int radius)
+        {
+            return Circle._Pi * radius * radius;
+        }
+    }
+}
